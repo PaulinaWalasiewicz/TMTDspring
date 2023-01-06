132296a31a0631834c8b9062a87861f5214708d4
PaulinaWalasiewicz <115495742+PaulinaWalasiewicz@users.noreply.github.com>
2023-01-06 23:27:25 +0100

    Connection do oracle DB, creating table user

---
+++ src/main/java/com/tmtd/tmtdspring/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package Repository;
+import java.util.List;
+
+import Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends  JpaRepository<User,Long>{
+    List<User> findByEmail(String email);
+}
