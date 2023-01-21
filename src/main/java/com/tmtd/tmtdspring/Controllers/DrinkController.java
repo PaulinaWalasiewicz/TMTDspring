6dcfedecab96b7f4b12420f35672439b92f28217
PaulinaWalasiewicz <115495742+PaulinaWalasiewicz@users.noreply.github.com>
2023-01-21 13:04:11 +0100

    controllers imports

--- src/main/java/com/tmtd/tmtdspring/Controllers/DrinkController.java
+++ src/main/java/com/tmtd/tmtdspring/Controllers/DrinkController.java
@@ -1,2 +1,25 @@
-package com.tmtd.tmtdspring.Controllers;public class DrinkController {
+package com.tmtd.tmtdspring.Controllers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import com.tmtd.tmtdspring.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+@RequestMapping("/api")
+public class DrinkController {
 }
