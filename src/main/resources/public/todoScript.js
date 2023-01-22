7c23f452474fef2d65cf58dd7570f1c482b5fd83
magdaTrzTrash <mwt.trash@gmail.com>
2023-01-22 17:24:15 +0100

    GET request work

--- src/main/resources/public/todoScript.js
+++ src/main/resources/public/todoScript.js
@@ -1,84 +1,111 @@
-        const idUser = "404";
-        function getTasks() {
-        fetch(`http://localhost:8080/api/users/${idUser}/tasks`)
-            .then(response => response.json())
-            .then(data => {
-                var ul = document.getElementById("taskList");
-                data.forEach(task => {
-                    console.log(task);
-                    console.log(task.title);
-                    var tasks = data;
-                    // document.querySelector("#tasks-items-container").innerHTML = task.title;
-                    var divTask = document.createElement("div");
-                    divTask.appendChild(document.createTextNode(task.title));
-                    divTask.appendChild(document.createTextNode(task.dueDate));
-                    divTask.appendChild(document.createTextNode(task.completed));
+let tasks = [];
+let users = [];
+let items = [];
+let idUser = '404';
 
-                    //create the edit button
-                    var editButton = document.createElement("button");
-                    editButton.innerHTML = "Edit";
-                    editButton.onclick = function() {
-                         editTask(task.id);
-                    };
-                    divTask.appendChild(editButton);
-                    
-                    //create the delete button
-                    // var deleteButton = document.createElement("button");
-                    // deleteButton.innerHTML = "Delete";
-                    // deleteButton.onclick = function() {
-                    //     deleteTask(task.id);
-                    // };
-                    // divTask.appendChild(deleteButton);
-                    //
-                    // //create the done button
-                    // var doneButton = document.createElement("button");
-                    // doneButton.innerHTML = "Done";
-                    // doneButton.onclick = function() {
-                    //     markTaskAsDone(task.id);
-                    // };
-                    // divTask.appendChild(doneButton);
-                    //
-                    ul.appendChild(divTask);
-                });
-            });
-    }
-
-        function editTask(taskId) {
-        var taskName = prompt("Enter new task name: ");
-        if (taskName) {
-        fetch(`http://localhost:8080/api/tasks/${taskId}`, {
-        method: "PUT",
-        body: JSON.stringify({ name: taskName }),
-        headers: { "Content-Type": "application/json" }
-    })
-        .then(response => response.json())
+function getTasks(idUser) {
+    fetch(`http://localhost:8080/api/users/${idUser}/tasks`)
+        .then(res => {
+            if(!res.ok){
+                console.log("problem");
+                return;
+            }
+            return res.json();
+        })
         .then(data => {
-        console.log("Task updated: ", data);
-        getTasks();
-    });
-    }
-    }
+            tasks = data;
+            console.log(tasks)
+            if(tasks && tasks.length > 0){
+                console.log("Załadowano taski do tablicy");
+            }
+            else{
+                console.log("No Tasks found");
+            }
+        })
+        .catch(error => {
+            console.log("error")
+        });
+}
+getTasks(idUser)
 
-        function deleteTask(taskId) {
-        if (confirm("Are you sure you want to delete this task?")) {
-        fetch(`http://localhost:8080/api/tasks/${taskId}`, {
-        method: "DELETE"
-    })
-        .then(response => response.json())
+function postTask(idUser) {
+    fetch(`http://localhost:8080/api/users/${idUser}/tasks`)
+        .then(res => {
+            if(!res.ok){
+                console.log("problem");
+                return;
+            }
+            return res.json();
+        })
         .then(data => {
-        console.log("Task deleted: ", data);
-        getTasks();
-    });
-    }
+            tasks = data;
+            console.log(tasks)
+            if(tasks && tasks.length > 0){
+                console.log("Załadowano taski do tablicy");
+            }
+            else{
+                console.log("No Tasks found");
+            }
+        })
+        .catch(error => {
+            console.log("error")
+        });
+}
+
+todoMain();
+function todoMain() {
+    let inputElem
+    let ulElem
+    getElements();
+    addListeners();
+
+    function getElements(){
+        inputElem = document.getElementsByTagName('input')[0];
+        ulElem = document.getElementsByTagName('ul')[0];
     }
 
-        // function markTaskAsDone(taskId) {
-        //     fetch("https://example.com/api/tasks/" + taskId + "/done", {
-        //         method: "PUT"
-        //     })
-        //         .then(response => response.json())
-        //         .then(data => {
-        //             console.log("Task marked as done: ", data);
-        //             getTasks();
-        //         });
-        // }
+    function addListeners(){
+        inputElem.addEventListener("change", onChange, false);
+    }
+
+    function onChange(event){
+        let inputValue = inputElem.value;
+        inputElem.value = "";
+        console.log(inputValue);
+        let liElem = document.createElement("li");
+        liElem.innerText = inputValue;
+        ulElem.appendChild(liElem);
+
+        const newTask = {
+            title: inputValue,
+            description: {
+                content: "descriptionContent"
+            },
+            startTime: "2016-03-04 11:08",
+            completed: false,
+            user: {
+                username: "admin",
+                password: "admin",
+                email: "dfghj",
+                firstName: "dfgh",
+                lastName: "sdfghj"
+            },
+            category: {
+                content: "categoryContent"
+            },
+            priority: {
+                content: "priorityContent"
+            }
+        };
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(newTask),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        fetch('http://localhost:8080/api/users/404/tasks', options)
+        console.log(newTask.value);
+    }
+}
